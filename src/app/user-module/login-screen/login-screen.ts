ba8528c69ee705b3e7048908c59fc502b9eb2f38
Eduardo Pereira <dudupererasiva@gmail.com>
2025-09-03 21:56:07 -0300

    baind do campo de email e password

--- src/app/user-module/login-screen/login-screen.ts
+++ src/app/user-module/login-screen/login-screen.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-screen',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-screen.html',
   styleUrl: './login-screen.css'
 })
 export class LoginScreen {
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    // Quando a tela iniciar
+
+    this.loginForm = this.fb.group({
+
+      email: ["", [Validators.required]],
+      password: ["", [Validators.required]]
+
+    });
+
+  }
+
+  onLoginClick() {
+
+    alert("Botão de login clicado")
+
+    console.log("email", this.loginForm.value.email);
+    console.log("password", this.loginForm.value.password);
+
+  }
 
 }
