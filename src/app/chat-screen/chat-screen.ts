11a990b98d60ca64c3ea60150a2084ca1406d4c1
Eduardo Pereira <dudupererasiva@gmail.com>
2025-09-16 21:46:42 -0300

    adcionei as funcionalidades dos botões de chat

--- src/app/chat-screen/chat-screen.ts
+++ src/app/chat-screen/chat-screen.ts
@@ -1,57 +1,99 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { firstValueFrom } from 'rxjs';
 
- interface IChat {
-  
-  chatTitle:string;
+interface IChat {
+
+  chatTitle: string;
   id: number;
   userId: string;
 
- }
+}
+
+interface IMenssage{
+
+  chatId: number;
+  id: number;
+  text: string;
+  userId: string;
+
+}
 
 @Component({
   selector: 'app-chat-screen',
-  imports: [HttpClientModule, CommonModule],
-  templateUrl:'./chat-screen.html',
+  imports: [CommonModule],
+  templateUrl: './chat-screen.html',
   styleUrl: './chat-screen.css'
 })
 export class ChatScreen {
 
-  chats : IChat[];
+  chats: IChat[];
+  chatSelecionado: IChat;
+  menssagens: IMenssage[];
 
-  constructor( private htpp:HttpClient){
+  constructor(private htpp: HttpClient, private cd: ChangeDetectorRef) {
     //inicialização de variaveis
 
     this.chats = [];
+    this.chatSelecionado = null!;
+    this.menssagens =[];
   }
 
-  ngOnInit(){
+  ngOnInit() {
     //buscar dados da api
 
     this.getChats();
-    
+
   }
 
-  async getChats(){
+  async getChats() {
     //busca os chats da api
 
-    let response = await this.htpp.get(" https://senai-gpt-api.azurewebsites.net/chats",{
-      headers:{
-        "Authorization" : "Bearer " + localStorage.getItem("meuToken")
+    // let response = await this.htpp.get(" https://senai-gpt-api.azurewebsites.net/chats",{
+    //   headers:{
+    //     "Authorization" : "Bearer " + localStorage.getItem("meuToken")
+    //   }
+    // }).toPromise();
+
+    let response = await firstValueFrom(this.htpp.get(" https://senai-gpt-api.azurewebsites.net/chats", {
+      headers: {
+        "Authorization": "Bearer " + localStorage.getItem("meuToken")
       }
-    }).toPromise();
+    }
+    ));
+
 
     console.log("Chats", response);
 
-    if(response){
+    if (response) {
 
-    this.chats = response as [];
+      this.chats = response as [];
 
-    } else{
+    } else {
 
       alert("error ao buscar os chats");
 
     }
+
+    this.cd.detectChanges();
+
+  }
+
+  async onChatClick(chatClicado: IChat) {
+
+    console.log("Chat Clicado", chatClicado);
+    this.chatSelecionado = chatClicado;
+
+    //logica para buscar as mensagens.
+    let response = await firstValueFrom(this.htpp.get(" https://senai-gpt-api.azurewebsites.net/messages?chatId=" + chatClicado.id, {
+      headers: {
+        "Authorization": "Bearer " + localStorage.getItem("meuToken")
+      }
+    }
+    ));
+
+    console.log("Menssagens", response)
+    this.menssagens = response as IMenssage[];
   }
 }
